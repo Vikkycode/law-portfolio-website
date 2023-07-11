766a91d30476879129cc6b6f7c8d1fce62b75acd
Victor <vicky.robilson5528@gmail.com>
2023-07-11 15:35:16 +0100

    law

---
+++ app/utils/index.tsx
@@ -0,0 +1,148 @@
+import aboutPage from '../about/page'
+
+export const NavLinks= [
+    {
+        title:'Home',
+        href:'/',
+    },
+    {
+        title:'Portfolio',
+        href:'/portfolio',
+    },
+    {
+        title:'Event',
+        href:'/event',
+    },
+    {
+        title:'Blog',
+        href:'/blog',
+    },
+    {
+        title:'Store',
+        href:'/store',  
+    },
+    {
+        title:'About',
+        href:'../about/',
+    },
+    {
+        title:'Contact',
+        href:'/contact',
+    }
+]
+
+
+export const PracticeItems2 = [
+    {
+        title:'CORPORATE & COMMERCIAL',
+    },
+    {
+        title:'INSOLVENCY & DEBT RECOVERY',
+    },
+    {
+        title:'SPECIAL PROJECTS & EMERGENCY ISSUES',
+    },
+    {
+        title:'TAXATION',
+    },
+    {
+        title:'REAL PROPERTY',
+    },
+    {
+        title:'OIL, GAS & ENVIRONMENTAL LAW',
+    },
+    {
+        title:'ENERGY, ELECTRICITY LAWS & REGULATIONS',
+    },
+    {
+        title:'FOREIGN INVESTMENT',
+    },
+    {
+        title:'BROADCASTING & TELECOMMUNICATIONS',
+    },
+    {
+        title:'AVIATION, MARITIME & PRACTICE',
+    },
+    {
+        title:'INTELLECTUAL PROPERTY',
+    },
+    {
+        title:'ICT CONSULTANT',
+    },
+
+]
+
+export const testimonialItems = [
+    {
+        img:'',
+        name:'Victor Frank',
+        content:'  Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit provident, molestias magnam modi id unde eius beatae? Minus, voluptatum quis.'
+    },
+    {
+        img:'',
+        name:'Victor Frank',
+        content:'  Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit provident, molestias magnam modi id unde eius beatae? Minus, voluptatum quis.'
+    },{
+        img:'',
+        name:'Victor Frank',
+        content:'  Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit provident, molestias magnam modi id unde eius beatae? Minus, voluptatum quis.'
+    },
+]
+
+export const FooterMenuLink= [
+    {
+        title:'Home',
+        link:'',
+    },
+    {
+        title:'Portfolio',
+        link:''
+    },
+    {
+        title:'Event',
+        link:'',
+    },
+    {
+        title:'Blog',
+        link:''
+    },
+    {
+        title:'Store',
+        link:''
+    },
+    {
+        title:'About',
+        link:''
+    },
+    {
+        title:'Contact',
+        link:''
+    },
+]
+
+export const socialMediaLinks= [
+    {
+        title:'Telegram',
+        link:'',
+    },
+    {
+        title:'Youtube',
+        link:''
+    },
+    {
+        title:'Twitter',
+        link:''
+    },
+    {
+        title:'Facebook',
+        link:''
+    },
+    {
+        title:'Instagram',
+        link:''
+    },
+    {
+        title:'Linkedin',
+        link:''
+    },
+]
